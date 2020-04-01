b8c3add4ff3d794f27fd2e0a4dfd77451c243012
FrostyGelato <zirconcrystal@outlook.com>
2020-03-31 22:59:31 -0700

    Added new classes

--- MySQLProgram/src/crud/Central.java
+++ MySQLProgram/src/crud/Central.java
@@ -10,7 +10,7 @@ public class Central {
 
 	public static void main(String[] args) {
 		Scanner loginInfo = new Scanner(System.in);
-		System.out.println("You need to log in.");
+		System.out.println("You need to log in before you can continue.");
 		System.out.println("Please enter your username:");
 		String username = loginInfo.nextLine();
 		System.out.println("Please enter your password:");
---
+++ MySQLProgram/src/crud/CreateEntry.java
@@ -0,0 +1,5 @@
+package crud;
+
+public class CreateEntry {
+
+}
