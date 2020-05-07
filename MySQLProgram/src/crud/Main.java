de26d197c2a335067c8bf22d11773dab03f02cab
FrostyGelato <zirconcrystal@outlook.com>
2020-05-07 07:38:12 -0700

    allowed the user to enter MySQL username and password

--- MySQLProgram/src/crud/Main.java
+++ MySQLProgram/src/crud/Main.java
@@ -8,22 +8,22 @@ import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
 
 public class Main {
-	public static final String DB_USER;
-	public static final String DB_PASS;
+	public static String DB_USER = "";
+	public static String DB_PASS = "";
 	
 	private static void createAndShowLoginGUI() {
 		Login loginGUI = new Login();
 		loginGUI.setVisible(true);
-    	}
+    }
+	
 	public static void main(String[] args) {
 		if (args.length < 2) {
 			System.out.println("Please provide the db user + password to run the program:\njava -jar <jarfile> <username> <password>");
 			System.exit(1);
 		} else {
-			this.DB_USER = args[0];
-			this.DB_PASS = args[1];
+			DB_USER = args[0];
+			DB_PASS = args[1];
 		}
-		
 		SwingUtilities.invokeLater(new Runnable() {
 	        public void run() {
 	        	createAndShowLoginGUI();
