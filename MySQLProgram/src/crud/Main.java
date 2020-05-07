1d45e2ac271b3a3854701ece3b3883e9e8149374
Shrimps <47574467+cocopower@users.noreply.github.com>
2020-05-07 20:21:58 +0800

    Oops, forgot to exit on invalid args

--- MySQLProgram/src/crud/Main.java
+++ MySQLProgram/src/crud/Main.java
@@ -18,6 +18,7 @@ public class Main {
 	public static void main(String[] args) {
 		if (args.length < 2) {
 			System.out.println("Please provide the db user + password to run the program:\njava -jar <jarfile> <username> <password>");
+			System.exit(1);
 		} else {
 			this.DB_USER = args[0];
 			this.DB_PASS = args[1];
