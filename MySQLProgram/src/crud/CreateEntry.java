e8bd27a957e17db59019667cdec9ee6ee3ef24b3
FrostyGelato <zirconcrystal@outlook.com>
2020-05-12 02:25:33 -0700

    added some more of CreateEntry

--- MySQLProgram/src/crud/CreateEntry.java
+++ MySQLProgram/src/crud/CreateEntry.java
@@ -1,7 +1,25 @@
 package crud;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
 public class CreateEntry {
-	public void CreateEntry(String bookName, String authorFirstName, String authorLastName, String ISBN, int quantity, int year, String publisher, String category) {
+	public void CreateEntry(String bookName, String authorFirstName, String authorLastName, String ISBN, String quantity, String year, String publisher, String category) {
 		String newEntry = "Insert into library (Book_name, Author_first_name, Author_last_name, ISBN, quantity, year, publisher, category)" + " values (?, ?, ?, ?, ?, ?, ?, ?)";
+		
+		try (Connection conn = DBConnection.getConnection(); PreparedStatement stmt = conn.prepareStatement(newEntry);) {
+			stmt.setString(1, bookName);
+			stmt.setString(2, authorFirstName);
+			stmt.setString(3, authorLastName);
+			stmt.setString(4, ISBN);
+			stmt.setString(5, quantity);
+			stmt.setString(6, year);
+			stmt.setString(7, publisher);
+			stmt.setString(8, category);
+				
+			stmt.execute();
+		} catch (Exception e) {
+				
+		}
 	}
 }
