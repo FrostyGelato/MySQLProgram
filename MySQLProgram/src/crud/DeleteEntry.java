bfd78cd06b63e9c9ec2b222ebd1ec360b44ff67b
FrostyGelato <zirconcrystal@outlook.com>
2020-05-17 23:19:29 -0700

    added DeleteEntry class and method

---
+++ MySQLProgram/src/crud/DeleteEntry.java
@@ -0,0 +1,19 @@
+package crud;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+public class DeleteEntry {
+
+	public DeleteEntry(String entryID) {
+		String entryToDelete = "delete from library where id = ?";
+	    try (Connection conn = DBConnect.getConnection(); PreparedStatement stmt = conn.prepareStatement(entryToDelete);) {
+	    	stmt.setString(1, entryID);
+				
+			stmt.execute();
+			conn.close();
+		} catch (Exception e) {
+				
+		}
+	}
+}
